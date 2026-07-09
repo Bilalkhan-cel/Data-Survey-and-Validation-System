503b50d3792c827457ebb6c63a7822e38563c388
Bilalkhan-cel <bilalahmedkhan6002@gmail.com>
2026-07-09 12:58:47 +0500

    r

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ with app.app_context():
 @app.after_request
 def allow_iframe(response):
     response.headers.pop("X-Frame-Options", None)
-    response.headers["Content-Security-Policy"] = "frame-ancestors 'self' *;"
+    response.headers["Content-Security-Policy"] = "frame-ancestors https:;"
     return response
 
 
